7d50706054d13e989f338c85fc53c19f48027607
fsvboas <felippesantosvb@hotmail.com>
2021-06-10 11:48:11 -0300

    Walkthrough

---
+++ Walkthrough/webapp/Component.js
@@ -0,0 +1,57 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel",
+	"./controller/HelloDialog",
+	"sap/ui/Device"
+
+], function (UIComponent, JSONModel, HelloDialog, Device) {
+
+	"use strict";
+	return UIComponent.extend("sap.ui.demo.walkthrough.Component", {
+		metadata: {
+			manifest: "json"
+		},
+		init: function () {
+			UIComponent.prototype.init.apply(this, arguments);
+
+			var oData = {
+				recipient: {
+					name: "World"
+				}
+			};
+			var oModel = new JSONModel(oData);
+			this.setModel(oModel);
+
+			this.getModel("invoice").setUseBatch(false);
+
+			// SETTING DEVICE MODEL
+			var oDeviceModel = new JSONModel(Device);
+			oDeviceModel.setDefaultBindingMode("OneWay");
+			this.setModel(oDeviceModel, "device");
+
+			this._helloDialog = new HelloDialog(this.getRootControl());
+
+			this.getRouter().initialize();
+		},
+
+		getContentDensityClass: function () {
+			if (!this._sContentDensityClass) {
+				if (!Device.support.touch) {
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		},
+
+		exit: function () {
+			this._helloDialog.destroy();
+			delete this._helloDialog;
+		},
+
+		openHelloDialog: function () {
+			this._helloDialog.open();
+		}
+	});
+});
